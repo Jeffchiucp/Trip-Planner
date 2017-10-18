e94067c5cc4ad8129385bc19f6d9bc2db0b57a50
elmerastudillo <astudilloelmer@icloud.com>
2017-10-18 00:00:00 -0700

    Implemented Service layer, models and View Controllers

---
+++ Trip-Planner/Trip-Planner/NetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  Trip-Planner
+//
+//  Created by Elmer Astudillo on 10/17/17.
+//  Copyright © 2017 Elmer Astudillo. All rights reserved.
+//
+
+import Foundation
---
+++ Trip-Planner/Trip-Planner/TripCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripCell.swift
+//  Trip-Planner
+//
+//  Created by Elmer Astudillo on 10/17/17.
+//  Copyright © 2017 Elmer Astudillo. All rights reserved.
+//
+
+import UIKit
+
+class TripCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
